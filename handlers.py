50e749fbec22e67fd2603794694833cb93129cfa
Ivan Zavgorodniy <izavgorodny@gmail.com>
2025-10-14 13:19:19 +0200

    Add import for CallbackQueryHandler

--- handlers.py
+++ handlers.py
@@ -9,6 +9,7 @@ from typing import Optional
 
 from telegram import InlineKeyboardButton, InlineKeyboardMarkup, Update
 from telegram.ext import (
+    CallbackQueryHandler,
     CommandHandler,
     ContextTypes,
     ConversationHandler,
